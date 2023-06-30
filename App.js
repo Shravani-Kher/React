514be47ea33b32953468db77d030edd9b5a44745
Shravani <skher462@gmail.com>
2023-07-01 00:31:04 +0530

    test

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-console.log("Shravani");
+const React = require("react");
+
+//import React from "react";
+//import ReactDOM from "react-dom/client";
+const ReactDOM = require("react-dom/client");
+
+const AppLayout = () => {
+  return React.createElement(React.Fragment, null);
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
